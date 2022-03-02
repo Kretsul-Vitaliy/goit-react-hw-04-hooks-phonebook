630f4a181bc472a4697869728246b0bd5224d2bb
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-03-02 18:21:52 +0200

    add task 1
    add localStorage

--- src/App/App.js
+++ src/App/App.js
@@ -6,16 +6,12 @@ import Section from "../components/Section";
 import ContactList from "../components/ContactList";
 import Filter from "../components/Filter";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
+import initialContacts from "../data/contacts.json";
 
 class App extends Component {
   // состояние. публичное свойство state. свойство экземпляра, всегда объект. от свойств этого объекта зависит разметка
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -64,6 +60,25 @@ class App extends Component {
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
+
+  // localStorage
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    } else {
+      this.setState({ contacts: initialContacts });
+    }
+    // console.log(parsedContacts);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
