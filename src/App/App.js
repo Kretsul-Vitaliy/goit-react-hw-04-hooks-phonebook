48806ac927de036027b9b12422584d6c4491ca6a
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-04-10 11:25:51 +0300

    refactoring hooks

--- src/App/App.js
+++ src/App/App.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 import { GlobalStyle } from "../theme/GlobalStyle.styled";
 import ContactForm from "../components/ContactForm";
 import Section from "../components/Section";
 import ContactList from "../components/ContactList";
 import Filter from "../components/Filter";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
+
 import initialContacts from "../data/contacts.json";
 
-class App extends Component {
-  // состояние. публичное свойство state. свойство экземпляра, всегда объект. от свойств этого объекта зависит разметка
-  state = {
-    contacts: [],
-    filter: "",
-  };
-
+function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem("contacts")) || initialContacts || []
+  );
+  const [filterContact, setFilterContact] = useState("");
   //функция (метод) обработки контактов которая добавляем новый контакт
-  handleAddContact = (newContact) =>
-    this.setState(
-      ({ contacts }) => ({
-        contacts: [...contacts, newContact],
-      }),
+  const handleAddContact = (newContact) => {
+    setContacts(
+      (prevContacts) => [...prevContacts, newContact],
       Notify.success("Contact is add phonebook")
     );
-
+  };
   //функция (метод) проверки на уникальность контактов
-  handleCheckUniqueContact = (name) => {
-    //берем наши контакты из state
-    const { contacts } = this.state;
+  const handleCheckUniqueContact = (name) => {
+    //берем наши контакты из Usestate
     //переменная которая проверяет существует ли контакт в массиве контактов
     //ставим !! если что то найдет то получим true в противном случае false
     const isExistContact = !!contacts.find((contact) => contact.name === name);
@@ -40,69 +36,51 @@ class App extends Component {
   //метод для удаления контактов, в него будет приходить id и отталкиваться от предыдущего состояния используеться колбек и
   // из деструктуризации наши контакты и их же будем возвращать используя метод filter на каждой итерации будет приходить контакт
   // и будем фильтровать id  (id который не равен id который мы хотим удалить)
-  handleRemoveContact = (id) =>
-    this.setState(
-      ({ contacts }) => ({
-        contacts: contacts.filter((contact) => contact.id !== id),
-      }),
+  const handleRemoveContact = (id) => {
+    setContacts(
+      (prevContacts) => prevContacts.filter((contact) => contact.id !== id),
       Notify.success("Contact is delete")
     );
+  };
   //обработчик фильтр handleFilterChange в него будет приходить фильтр и будем менять наш стейт filter
-  handleFilterChange = (filter) => this.setState({ filter });
+  const handleFilterChange = (filter) => setFilterContact(filter);
 
   //метод фильтрации контактов , будем брать из нашего state contacts и filter, будем возвращать отфильтрованный список контактов
   //при этом не будем изменять тот который находиться у нас в state, методом фильтр на каждой итерации приходит контакт и делаем проверку контакта
   // который будет содержать символы которые находяться в нашем фильтре но мы не должны привязываться к регистру, то что вводим большие или маленькие символы
   //приводим все в нижний регистр методом toLowerCase().includes(filter.toLowerCase()) и фильтр тоже к нижнему регистру
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
+  const getVisibleContacts = () => {
     return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+      contact.name.toLowerCase().includes(filterContact.toLowerCase())
     );
   };
 
-  // localStorage
-
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    } else {
-      this.setState({ contacts: initialContacts });
+  useEffect(() => {
+    if (contacts) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
     }
-    // console.log(parsedContacts);
-  }
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <>
-        <GlobalStyle />
-        <Section title="Phonebook">
-          <ContactForm
-            onAdd={this.handleAddContact}
-            onCheckUnique={this.handleCheckUniqueContact}
-          />
-        </Section>
-        <Section title="Contacts">
-          <h3>Find contacts by name</h3>
-          <Filter filter={filter} onChange={this.handleFilterChange} />
-          <ContactList
-            contacts={visibleContacts}
-            onRemove={this.handleRemoveContact}
-          />
-        </Section>
-      </>
-    );
-  }
+  // const visibleContacts = getVisibleContacts();
+  return (
+    <>
+      <GlobalStyle />
+      <Section title="Phonebook">
+        <ContactForm
+          onAdd={handleAddContact}
+          onCheckUnique={handleCheckUniqueContact}
+        />
+      </Section>
+      <Section title="Contacts">
+        <h3>Find contacts by name</h3>
+        <Filter filter={filterContact} onChange={handleFilterChange} />
+        <ContactList
+          contacts={getVisibleContacts()}
+          onRemove={handleRemoveContact}
+        />
+      </Section>
+    </>
+  );
 }
 
 export default App;
